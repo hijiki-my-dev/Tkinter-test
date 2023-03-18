65ab561f7f502c7f0370b404b5af8eb99c885298
Hijiki <hijikiblog01@gmail.com>
2023-03-19 00:51:34 +0900

    ボタンを追加

--- main.py
+++ main.py
@@ -3,13 +3,29 @@ from tkinter import *
 def main():
     root = Tk()
 
-    myLabel = Label(root,
-                    text = "Hello World!",
-                    foreground="#33589A",
-                    width=10,
-                    height=10)
+    #ラベルウィジェット（クラス）画面にテキストや画像を表示する。
+    mylabel = Label(
+        root,
+        text = "Hello World!",
+        foreground="#33589A",
+        width=10, #幅と高さは数字の0の幅と高さを基準にしている
+        height=10
+    )
+    
+    #ボタン。ラベルとの違いはクリックできるかどうか
+    mybutton = Button(
+        text="Click me!",
+        width=25,
+        height=5,
+        #bg = "#33589A",
+        fg = "#33589A"
+    )
 
-    myLabel.pack()
+
+    mylabel.pack()
+    mybutton.pack()
+    
+    #root.configure(bg = "black")
 
     root.mainloop()
     
